2d382fdd133a2d5146076413e59617098d08ce5d
Felipe Espinosa <fn.espinosa@gmail.com>
2013-02-26 20:49:47 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Isell::Application.config.secret_token = '53ac234feb391042288558874dba590f914cc1e95356c823eda30ef5c350569b703f2b5ebad2fa7074273870e240bd6711b80b6a51c0870b241baa204b484fcf'
